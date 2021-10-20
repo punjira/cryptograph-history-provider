e6c21ce8113b5f9141b86466692e0ed77054c6b3
punjira <saeedwmail@gmail.com>
2021-10-20 15:52:14 +0330

    add binance candle request helper

---
+++ src/service/binance.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import {
+     binanceBaseAddress,
+     binanceCandleSticks,
+     binancePrefix,
+} from '../endpoints';
+
+// from binance docs
+export type BinanceKLineResponse = [
+     number, // Open time
+     string, // Open
+     string, // High
+     string, // Low
+     string, // Close
+     string, // Volume
+     number, // Close time
+     string, // Quote asset volume
+     number, // Number of trades
+     string, // Taker buy base asset volume
+     string, // Taker buy quote asset volume
+     string // Ignore.
+];
+
+export function getBinanceKLines(
+     symbol: string,
+     interval: string,
+     limit = 2
+): Promise<(number | string)[][]> {
+     return new Promise((resolve, reject) => {
+          const options: AxiosRequestConfig = {
+               method: 'GET',
+               url: binanceBaseAddress + binancePrefix + binanceCandleSticks,
+               params: {
+                    symbol: symbol.toUpperCase(),
+                    interval,
+                    limit,
+               },
+          };
+          axios(options)
+               .then((data: AxiosResponse<BinanceKLineResponse[]>) => {
+                    const stripped = data.data.map((el) => el.slice(0, 7));
+                    return resolve(stripped);
+               })
+               .catch((err) => {
+                    return reject(err);
+               });
+     });
+}
