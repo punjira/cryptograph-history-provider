b7041d44ba52b5262db26c1677a2f60b46f3005d
punjira <saeedwmail@gmail.com>
2021-10-28 19:28:23 +0330

    schedule price update

--- src/lib/binance-schedule-coins.ts
+++ src/lib/binance-schedule-coins.ts
@@ -1,8 +1,19 @@
 import schedule from 'node-schedule';
 import { updateFromBinance } from './binance-interval';
+import { getBinanceTickerPrice } from './binance-price-interval';
 
 export function createSchedule() {
-     const job = schedule.scheduleJob('*/10 * * * * *', function () {
-          updateFromBinance(['btcusdt', 'ethusdt', 'trxusdt']);
+     const candleJob = schedule.scheduleJob('*/59 * * * * *', function () {
+          updateFromBinance([
+               'btcusdt',
+               'ethusdt',
+               'trxusdt',
+               'ltcusdt',
+               'shibusdt',
+          ]);
      });
+     const priceJob = schedule.scheduleJob('*/5 * * * * *', function () {
+          getBinanceTickerPrice();
+     });
+     // really getting tempted to call something BoobJob here!
 }
