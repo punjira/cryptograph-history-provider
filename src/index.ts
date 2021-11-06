c22b02321a82f5b71d864094b65fbbccd0f37ab6
punjira <saeedwmail@gmail.com>
2021-11-06 09:33:21 -0400

    remove old schedule

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ import cors from 'cors';
 app.use(cors());
 require('./database/mongo');
 require('./nats/subscription');
-import { redisClient } from './redis/redis-clinet';
+// import { redisClient } from './redis/redis-clinet';
 
-redisClient.getInstance().getClient();
+// redisClient.getInstance().getClient();
 
-import { feedInitialData } from './lib/binance-initial-history';
-import { createSchedule } from './lib/binance-schedule-coins';
-feedInitialData();
-createSchedule();
+// import { feedInitialData } from './lib/binance-initial-history';
+// import { createSchedule } from './lib/binance-schedule-coins';
+// feedInitialData();
+// createSchedule();
+import manager from './jobs/manager';
+manager();
 
 import historyRoutes from './routes/history-routes';
 import priceRoutes from './routes/price-routes';
