fecde77cf16a0e5dd42bc0e4e924565c86b1d384
punjira <saeedwmail@gmail.com>
2021-11-07 05:09:00 -0500

    clean database connection

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,12 +1,12 @@
-import { DatabaseConnectionError } from '@cryptograph-app/error-handlers';
 import mongoose from 'mongoose';
 import { logger, LOG_LEVELS } from '../../winston';
-let connected: boolean = false;
 
 export const MongoConnect = function (callback) {
      console.log('creating mongodb connection');
-     mongoose.connect(`${process.env.MONGO_URL}/study`, {
-          connectTimeoutMS: 3000,
+     mongoose.connect(`${process.env.MONGO_URL}`, {
+          dbName: 'history',
+          connectTimeoutMS: 60000,
+          socketTimeoutMS: 60000,
           keepAlive: true,
      });
      const db = mongoose.connection;
@@ -20,44 +20,11 @@ export const MongoConnect = function (callback) {
                'error connecting to database , error description: ' + err,
                'database/mongo.ts'
           );
-          console.log('=======================================');
-          console.log('mongo connections error, Error: ', err);
-          throw new DatabaseConnectionError();
      });
-     db.on('disconnected', (thing) => {
-          console.log('========================================');
-          console.log('mongo connection disconnected, The thing: ', thing);
-          // mongoose.connect(`${process.env.MONGO_URL}/study`, {
-          //      connectTimeoutMS: 3000,
-          //      keepAlive: true,
-          // });
-     });
-     db.on('disconnecting', (thing) => {
-          console.log('========================================');
-          console.log('mongo connection disconnecting, The thing: ', thing);
-     });
-     db.on('close', (thing) => {
-          console.log('========================================');
-          console.log('mongo connection disconnecting, The thing: ', thing);
+     db.on('disconnected', () => {
+          mongoose.connect(`${process.env.MONGO_URL}/history`, {
+               connectTimeoutMS: 3000,
+               keepAlive: true,
+          });
      });
 };
-
-// (async function () {
-//      if (connected) {
-//           return;
-//      }
-//      await mongoose.connect(`${process.env.MONGO_URL}/history`);
-//      const db = mongoose.connection;
-//      db.once('open', () => {
-//           console.log('connection to mongo db created');
-//           connected = true;
-//      });
-//      db.on('error', (err) => {
-//           logger(
-//                LOG_LEVELS.ERROR,
-//                'error connecting to database , error description: ' + err,
-//                'database/mongo.ts'
-//           );
-//           throw new DatabaseConnectionError();
-//      });
-// })();
